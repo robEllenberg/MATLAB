4b52de91c5bd4811f1ee6e9c5bcb4e267b6b87d5
Robert Ellenberg <rwe24@drexel.edu>
2011-11-16 14:21:53 -0500

    Deal with normal vectors in import

--- vrml/importVRMLMesh.m
+++ vrml/importVRMLMesh.m
@@ -1,4 +1,4 @@
-function [pointcloud,K]=importVRMLMesh(fname,dispResult)
+function data=importVRMLMesh(fname,dispResult)
 %% importVRMLMesh
 % Author: Robert Ellenberg
 %   (Based on wrl2mat by Alireza Bossaghzadeh)
@@ -11,34 +11,25 @@ function [pointcloud,K]=importVRMLMesh(fname,dispResult)
 
 vrfile=fopen(fname);
 
-fprintf('Reading pointcloud data from %s\n', fname)
-p=1;
-counter=0;
 pointcloud=zeros(2000,3);
+coordIndex=zeros(2000,4);
+foundData=0;
+types={'point','coordIndex','vector'};
 
-while counter ~= -1
-    data=fgets(vrfile);
-    fpoint=strfind(data,'point');% 2 checkers to find out the begining
-    f2point=strfind(data,'[');   %of the x,y,z " point [ "
-    while ~isempty(fpoint) && ~isempty(f2point)
-        data=fgets(vrfile);
-        if isempty(strfind(data,']'));
-            t=sscanf(data,'%f %f %f');
-            pointcloud(p,:)=t';
-            p=p+1;
-        else fpoint=[];counter=-1;
-        end
-        if p>size(pointcloud,1);
-            pointcloud=[pointcloud;zeros(200,3)];
-        end
-    end
-end
+while ~feof(vrfile)
+    [datatype,foundData]=findVRMLData(vrfile);
+    %TODO extratc each type of data and store it appropriately
+    %TODO change the other functions to use this new structure
+    %cell array format
+    data{k}=extractVRMLData(vrfile)
+
+    
+
 
 pointcloud=pointcloud(1:p,:); %trim padded zeros
 
 p=1;
 counter=0;
-coordIndex=zeros(2000,4);
 fprintf('Reading surface mesh data from %s\n', fname)
 while counter ~= -1
     data=fgets(vrfile);
@@ -77,3 +68,48 @@ if dispResult
    
 end
 
+function [datatype,foundType]=findVRMLData(fidi,types)
+
+    if nargin<2
+        types={'point','coordIndex','vector'};
+    end
+
+    foundType=0;
+    dataype='none';
+
+    while ~feof(fid) || ~foundType
+        data=fgetl(vrfile);
+        for k=1:length(types)
+            %TODO: check that this find the headings correctly
+            if ~isempty(strfind(data,types{k})) && ~isempty(strfind(data,'['))
+                datatype=types{k}
+                foundType=1;
+                break;
+            end
+        end
+    end
+end
+
+function data=extractVRMlData(fid)
+    %Extract data from the current location to the end of the vector
+    %TODO test this
+
+    dataLine=fgets(fid);
+    t=sscanf(dataLine,'%f');
+    data=zeros(1000,length(t));
+
+    scanComplete=0;
+
+    while ~scanComplete 
+        t=sscanf(dataLine,'%f'); %Scan for data
+        data(p,:)=t'; %Store the line of data
+        p=p+1;  
+
+        if p>size(data,1);
+            data=[data;zeros(200,4)];
+        end
+        %Read the next line
+        dataLine=fgets(fid);
+        scanComplete=~isempty(strfind(dataLine,']'));
+    end
+end
