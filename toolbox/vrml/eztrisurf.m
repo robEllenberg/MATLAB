f4a3f0ac3cb4512c7b03f8ebb830896ec6aaae14
Robert Ellenberg <rwe24@drexel.edu>
2013-03-03 20:11:43 -0500

    Moved VRML stuff to submodule

--- toolbox/vrml/eztrisurf.m
+++
@@ -1,4 +0,0 @@
-function h=eztrisurf(K,P)
-    h=trisurf(K,P(:,1),P(:,2),P(:,3));
-    axis equal
-end
--- toolbox/vrml/testvrml.m
+++
@@ -1,6 +0,0 @@
-fname='Body_LPalm.wrl'
-disp('Loading example VRML')
-importVRMLMesh(fname,1)
-
-disp('Create mirror and convex hull')
-processVRML(fname,'LR',1,1)
